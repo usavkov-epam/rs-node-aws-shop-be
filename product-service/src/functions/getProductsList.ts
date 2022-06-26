1de0dfdc004535fe96a3b4e81989a2d480044e45
Yury Saukou <yury_saukou@epam.com>
2022-06-26 19:21:46 +0400

    Refactor code: add controller and model, apply serverless bundle plugin

--- product-service/src/functions/getProductsList.ts
+++ product-service/src/functions/getProductsList.ts
@@ -4,7 +4,6 @@ export const handler = async (event: any, _context: any) => {
   const { routeKey } = event;
 
   console.log(`LOGGER path - ${routeKey}`);
-  
 
   const allProducts = await getAllProducts();
 
