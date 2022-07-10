b3002cb34d5585f93b7adc3797dff71b9262f2af
Yury Saukou <yury_saukou@epam.com>
2022-07-10 21:12:57 +0400

    Add unit test for catalogBatchProcess

---
+++ import-service/tests/fixtures/records.ts
@@ -0,0 +1,16 @@
+export const queueRecords = [
+  {
+    id: "7785455e-6a8c-4111-a532-9a18f8aa22ca",
+    title: "iPhone 11",
+    description: "Apple iPhone 11",
+    price: 699,
+    count: 16,
+  },
+  {
+    id: "7785455e-6a8c-4111-a532-9a18f8aa22ca",
+    title: "iPhone 12",
+    description: "Apple iPhone 12",
+    price: 799,
+    count: 16,
+  },
+]
